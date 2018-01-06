d5d38507ea3fd0041b66e48f3833ecc342ba25d9
Nicolas Chen <35080480+nicolaschen1@users.noreply.github.com>
2018-01-06 01:14:56 +0100

    Initial commit

---
+++ ShadowZip/Form1.Designer.cs
@@ -0,0 +1,355 @@
+﻿namespace ShadowZip
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.BrowserTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.AddPasswordButton = new System.Windows.Forms.Button();
+            this.SelectedFileList = new System.Windows.Forms.ListBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearAllToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.instructionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button3 = new System.Windows.Forms.Button();
+            this.DestinationTextBox = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ZipTextBox = new System.Windows.Forms.TextBox();
+            this.IsPassword = new System.Windows.Forms.CheckBox();
+            this.ShadowZipButton = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.PasswordTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // BrowserTextBox
+            // 
+            this.BrowserTextBox.Location = new System.Drawing.Point(14, 50);
+            this.BrowserTextBox.Name = "BrowserTextBox";
+            this.BrowserTextBox.Size = new System.Drawing.Size(189, 20);
+            this.BrowserTextBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Select your file";
+            // 
+            // button1
+            // 
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(216, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(28, 23);
+            this.button1.TabIndex = 2;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // AddPasswordButton
+            // 
+            this.AddPasswordButton.Enabled = false;
+            this.AddPasswordButton.Image = ((System.Drawing.Image)(resources.GetObject("AddPasswordButton.Image")));
+            this.AddPasswordButton.Location = new System.Drawing.Point(187, 106);
+            this.AddPasswordButton.Name = "AddPasswordButton";
+            this.AddPasswordButton.Size = new System.Drawing.Size(28, 23);
+            this.AddPasswordButton.TabIndex = 3;
+            this.AddPasswordButton.UseVisualStyleBackColor = true;
+            this.AddPasswordButton.Click += new System.EventHandler(this.AddPasswordButton_Click);
+            // 
+            // SelectedFileList
+            // 
+            this.SelectedFileList.FormattingEnabled = true;
+            this.SelectedFileList.Location = new System.Drawing.Point(14, 83);
+            this.SelectedFileList.Name = "SelectedFileList";
+            this.SelectedFileList.Size = new System.Drawing.Size(230, 95);
+            this.SelectedFileList.TabIndex = 4;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.helpToolStripMenuItem,
+            this.helpToolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(563, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.clearAllToolStripMenuItem,
+            this.quitToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.helpToolStripMenuItem.Text = "Menu";
+            // 
+            // clearAllToolStripMenuItem
+            // 
+            this.clearAllToolStripMenuItem.Name = "clearAllToolStripMenuItem";
+            this.clearAllToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.A)));
+            this.clearAllToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.clearAllToolStripMenuItem.Text = "Clear all";
+            this.clearAllToolStripMenuItem.Click += new System.EventHandler(this.clearAllToolStripMenuItem_Click);
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.F4)));
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.quitToolStripMenuItem.Text = "Quit";
+            this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem1
+            // 
+            this.helpToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.instructionsToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem1.Name = "helpToolStripMenuItem1";
+            this.helpToolStripMenuItem1.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem1.Text = "Help";
+            // 
+            // instructionsToolStripMenuItem
+            // 
+            this.instructionsToolStripMenuItem.Name = "instructionsToolStripMenuItem";
+            this.instructionsToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
+            this.instructionsToolStripMenuItem.Text = "Instructions";
+            this.instructionsToolStripMenuItem.Click += new System.EventHandler(this.instructionsToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // button3
+            // 
+            this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
+            this.button3.Location = new System.Drawing.Point(14, 194);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(230, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Delete selected item";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // DestinationTextBox
+            // 
+            this.DestinationTextBox.Location = new System.Drawing.Point(14, 263);
+            this.DestinationTextBox.Name = "DestinationTextBox";
+            this.DestinationTextBox.Size = new System.Drawing.Size(189, 20);
+            this.DestinationTextBox.TabIndex = 7;
+            // 
+            // button4
+            // 
+            this.button4.Image = ((System.Drawing.Image)(resources.GetObject("button4.Image")));
+            this.button4.Location = new System.Drawing.Point(216, 260);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(28, 23);
+            this.button4.TabIndex = 8;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.SelectedFileList);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.DestinationTextBox);
+            this.groupBox1.Controls.Add(this.BrowserTextBox);
+            this.groupBox1.Location = new System.Drawing.Point(12, 27);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(271, 303);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Configuration";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 247);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(154, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Select your destination location";
+            // 
+            // ZipTextBox
+            // 
+            this.ZipTextBox.Location = new System.Drawing.Point(24, 50);
+            this.ZipTextBox.Name = "ZipTextBox";
+            this.ZipTextBox.Size = new System.Drawing.Size(157, 20);
+            this.ZipTextBox.TabIndex = 10;
+            // 
+            // IsPassword
+            // 
+            this.IsPassword.AutoSize = true;
+            this.IsPassword.Location = new System.Drawing.Point(24, 83);
+            this.IsPassword.Name = "IsPassword";
+            this.IsPassword.Size = new System.Drawing.Size(103, 17);
+            this.IsPassword.TabIndex = 0;
+            this.IsPassword.Text = "Add a Password";
+            this.IsPassword.UseVisualStyleBackColor = true;
+            this.IsPassword.CheckedChanged += new System.EventHandler(this.IsPassword_CheckedChanged);
+            // 
+            // ShadowZipButton
+            // 
+            this.ShadowZipButton.Image = ((System.Drawing.Image)(resources.GetObject("ShadowZipButton.Image")));
+            this.ShadowZipButton.Location = new System.Drawing.Point(24, 154);
+            this.ShadowZipButton.Name = "ShadowZipButton";
+            this.ShadowZipButton.Size = new System.Drawing.Size(191, 129);
+            this.ShadowZipButton.TabIndex = 11;
+            this.ShadowZipButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.ShadowZipButton.UseVisualStyleBackColor = true;
+            this.ShadowZipButton.Click += new System.EventHandler(this.ShadowZipButton_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.PasswordTextBox);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.ZipTextBox);
+            this.groupBox2.Controls.Add(this.AddPasswordButton);
+            this.groupBox2.Controls.Add(this.IsPassword);
+            this.groupBox2.Controls.Add(this.ShadowZipButton);
+            this.groupBox2.Location = new System.Drawing.Point(309, 27);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(239, 303);
+            this.groupBox2.TabIndex = 12;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "ZipPassword";
+            // 
+            // PasswordTextBox
+            // 
+            this.PasswordTextBox.Enabled = false;
+            this.PasswordTextBox.Location = new System.Drawing.Point(24, 106);
+            this.PasswordTextBox.MaxLength = 14;
+            this.PasswordTextBox.Name = "PasswordTextBox";
+            this.PasswordTextBox.PasswordChar = '*';
+            this.PasswordTextBox.Size = new System.Drawing.Size(157, 20);
+            this.PasswordTextBox.TabIndex = 16;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(21, 34);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(99, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Name of the Zip file";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(225, 360);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "ShadowZip | Nicolas CHEN";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(563, 382);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "ShadowZip";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox BrowserTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button AddPasswordButton;
+        private System.Windows.Forms.ListBox SelectedFileList;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox DestinationTextBox;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox ZipTextBox;
+        private System.Windows.Forms.CheckBox IsPassword;
+        private System.Windows.Forms.Button ShadowZipButton;
+        private System.Windows.Forms.ToolStripMenuItem clearAllToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem instructionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox PasswordTextBox;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ ShadowZip/Form1.cs
@@ -0,0 +1,237 @@
+﻿/*
+SHADOWZIP
+VERSION: 1.0
+
+Inputs: Files
+
+Outputs: ZIP file
+
+Description: This software tool allows to make a ZIP file (archiving and data compression).
+
+Developer: Nicolas CHEN
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using Ionic.Zip;
+using System.Text;
+
+namespace ShadowZip
+{
+    public partial class Form1 : Form
+    {
+        List<string> listSelectedFile = new List<string>();
+        string zipPassword;
+        string zipName;
+        string zipDestinationPath;
+        bool zipStatus;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }      
+
+		/*** BUTTONS ***/
+		//File Dialog Button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                BrowserTextBox.Enabled = true;
+                BrowserTextBox.Text = openFileDialog1.FileName.ToString();
+
+                SelectedFileList.Items.Add(openFileDialog1.FileName);
+            }
+        }
+		
+		//Button to delete an item
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SelectedFileList.Items.RemoveAt(SelectedFileList.SelectedIndex);
+        }
+		
+        //Folder Browser Dialog Button for the destination location
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                DestinationTextBox.Enabled = true;
+                DestinationTextBox.Text = folderBrowserDialog1.SelectedPath.ToString();
+            }
+        }
+
+        //Button to make a ZIP file
+        private void ShadowZipButton_Click(object sender, EventArgs e)
+        {
+            foreach (object item in SelectedFileList.Items)
+                listSelectedFile.Add(item.ToString());
+
+            zipDestinationPath = DestinationTextBox.Text;
+            zipName = ZipTextBox.Text;
+
+            if (VerifyTextBox(BrowserTextBox.Text, PasswordTextBox.Text, ZipTextBox.Text, DestinationTextBox.Text))
+            {
+                zipStatus = RunShadowZip(listSelectedFile, zipPassword, zipDestinationPath, zipName);
+
+                if (zipStatus == true)
+                {
+                    MessageBox.Show("ZIP COMPLETED.", "Success");
+                    Reset();
+                }
+                else
+                {                    
+                    MessageBox.Show("ZIP FAILED.", "Fail", MessageBoxButtons.OK, 
+                        MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button1);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please fill all the blanks.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button1);
+                listSelectedFile.Clear();
+            }
+        }
+		
+		/*** MENU BAR ***/
+        private void clearAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Reset();
+        }
+
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();            
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string[] about = new string[]
+            {
+                "ShadowZip"
+                , ""
+                , "VERSION: 1.0"
+                , ""
+                , "Developed by Nicolas Chen"
+            };
+
+            MessageBoxMultiLines(about);
+        }
+
+        private void instructionsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string[] instructions = new string[] 
+            {
+                "SHADOWZIP is a software tool allows to make a ZIP file (archiving and data compression). "
+                , ""
+                , "1) Select your file. If there are several files, add these files one by one. "
+                , ""
+                , "2) Select your destination location."
+                , ""
+                , "3) Enter a name for the Zip file."
+                , ""
+                , "4) Click on the ZIP button."
+                , ""
+                , "Password Mode: If a password must be added in the ZIP file follow the steps below:"
+                , ""
+                , "a) Check 'Add a Password' to activete Password Mode."
+                , ""
+                , "b) Type your password (password is hidden by '*' and limited to 14 characters)."
+                , ""
+                , "c) Click on the padlock to assign your password."
+            };
+
+            MessageBoxMultiLines(instructions);
+        }
+		
+		
+		/*** METHODS ***/
+        //Verify if all textboxes are filled
+        public bool VerifyTextBox(String BrowserTextBox, String PasswordTextBox, String ZipTextBox, String DestinationTextBox)
+        {
+            return !(String.IsNullOrEmpty(BrowserTextBox) || (String.IsNullOrEmpty(PasswordTextBox) && IsPassword.Checked) || String.IsNullOrEmpty(ZipTextBox) || String.IsNullOrEmpty(DestinationTextBox));
+        }
+
+        //Reset all
+        public void Reset()
+        {
+            SelectedFileList.Items.Clear();
+            listSelectedFile.Clear();
+            ZipTextBox.Text = String.Empty;
+            DestinationTextBox.Text = String.Empty;
+            PasswordTextBox.Text = String.Empty;
+            BrowserTextBox.Text = String.Empty;
+            IsPassword.Checked = false;
+        }
+
+        //Make the ZIP file
+        public static bool RunShadowZip(List<string> listSelectedFile, string zipPassword, string DestinationPath, string ZipName)
+        {
+            if (listSelectedFile != null)
+            {
+
+                using (ZipFile shadowZip = new ZipFile())
+                {
+                    shadowZip.Password = zipPassword;
+                    foreach (var sFile in listSelectedFile)
+                    {
+                        shadowZip.AddFile(sFile, @"\"); //permet d'avoir le fichier sans les dossiers                        
+                    }
+                    shadowZip.Save(DestinationPath + "\\" + ZipName + ".zip");
+
+                    return true;
+                }
+            }
+            else
+                return false;
+        }
+
+        //Password Button
+        private void AddPasswordButton_Click(object sender, EventArgs e)
+        {
+            zipPassword = PasswordTextBox.Text;
+
+            if (String.IsNullOrEmpty(zipPassword))
+            {
+                PasswordTextBox.Enabled = true;
+                MessageBox.Show("Please enter a password.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button1);
+            }
+            else
+            {
+                PasswordTextBox.Enabled = false;
+                MessageBox.Show("Password is assigned.", "Password Mode Enabled");
+            }
+        }
+
+        //Password Mode
+        private void IsPassword_CheckedChanged(object sender, EventArgs e)
+        {
+            if (IsPassword.Checked)
+            {
+                PasswordTextBox.Enabled = true;
+                AddPasswordButton.Enabled = true;
+
+                MessageBox.Show("Enter a password and please activate it by clicking on the padlock.", "Password Mode");
+            }
+            else
+            {
+                PasswordTextBox.Enabled = false;
+                AddPasswordButton.Enabled = false;
+            }
+        }
+		
+		public void MessageBoxMultiLines(IEnumerable<string> lines)
+        {
+            var instructionLine = new StringBuilder();
+            bool firstLine = false;
+            foreach (string line in lines)
+            {
+                if (firstLine)
+                    instructionLine.Append(Environment.NewLine);
+                
+                instructionLine.Append(line);               
+                firstLine = true;
+            }
+            MessageBox.Show(instructionLine.ToString(), "Information");
+        }
+    }
+}
